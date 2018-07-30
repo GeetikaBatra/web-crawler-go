4b0f8b9fff06e6ff992f29959e298448050e5362
GeetikaBatra <gbatra@redhat.com>
2018-07-30 14:16:45 +0530

    Initial commit

---
+++ crawlServer/crawl1.go
@@ -0,0 +1,22 @@
+import(
+   "fmt"
+   // "github.com/streadway/amqp"
+)
+
+// func crawl(url string) []string{
+// 	fmt.Println(url)
+// 	list, err := links.Extract(url)
+
+// 	if err != null
+// }
+
+func main() {
+	// conn, err := amqp.Dial("amqp://guest:guest@crawl-broker:5672/")
+
+	// failOnError(err, "Failed to connect to RabbitMQ")
+
+	// defer conn.Close()
+	fmt.Prntln("*******************	working")
+	for {}
+
+}
